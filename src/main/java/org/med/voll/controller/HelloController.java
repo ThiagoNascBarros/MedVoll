d84f8e13c9b6a730c6ebcdca791aef80ce9edf98
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-22 16:18:53 -0300

    test: Testing controller with message "Hello Word!"

---
+++ src/main/java/org/med/voll/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.med.voll.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/helloword")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
