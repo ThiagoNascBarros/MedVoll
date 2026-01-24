f6cd72eca3aae6c2f31d3467859cfc27e73b35e6
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-24 19:48:09 -0300

    feat: Configurando o stateless

---
+++ src/main/java/org/med/voll/infra/security/SecurityConfigurations.java
@@ -0,0 +1,23 @@
+package org.med.voll.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean // Configurando padrão de autenticação - Usando a autenticação stateless
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(ses -> ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
