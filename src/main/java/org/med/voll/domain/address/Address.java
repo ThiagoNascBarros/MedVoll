859879faa7469c7f98146b104811652caedae8e8
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-23 21:02:04 -0300

    feat: Criando sobrecarga de função para atualizar dados do paciente

--- src/main/java/org/med/voll/domain/address/Address.java
+++ src/main/java/org/med/voll/domain/address/Address.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.med.voll.application.communication.request.doctor.RequestRegisterDoctorAddress;
 import org.med.voll.application.communication.request.patient.RequestRegisterPatientAddress;
+import org.med.voll.application.communication.request.patient.RequestUpdatePatient;
+import org.med.voll.application.communication.request.patient.RequestUpdatePatientAddress;
 
 @Getter
 @NoArgsConstructor
@@ -42,6 +44,36 @@ public class Address {
     }
 
     public void updatingInfo(RequestRegisterDoctorAddress endereco) {
+        if (endereco.logradouro() != null) {
+            this.logradouro = endereco.logradouro();
+        }
+
+        if (endereco.bairro() != null) {
+            this.bairro = endereco.bairro();
+        }
+
+        if (endereco.cep() != null) {
+            this.cep = endereco.cep();
+        }
+
+        if (endereco.numero() != null) {
+            this.numero = endereco.numero();
+        }
+
+        if (endereco.complemento() != null) {
+            this.complemento = endereco.complemento();
+        }
+
+        if (endereco.cidade() != null) {
+            this.cidade = endereco.cidade();
+        }
+
+        if (endereco.uf() != null) {
+            this.uf = endereco.uf();
+        }
+    }
+
+    public void updatingInfo(RequestUpdatePatientAddress endereco) {
         if (endereco.logradouro() != null) {
             this.logradouro = endereco.logradouro();
         }
